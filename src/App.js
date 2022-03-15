6e274a8b7a84f51b4d463d916d46e0112ce3c4a9
J-NAVEEN-KUMAR <naveen.jeruban@gmail.com>
2022-03-15 10:16:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Categories from "./Components/Categories/Categories";
+import Random from "./Components/Random/Random";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <h1>Hello World!</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/random" element={<Random />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
