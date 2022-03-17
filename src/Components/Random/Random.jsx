0dce364f7c6cf8cea1fd98ff76b2274c64ba8a68
J-NAVEEN-KUMAR <naveen.jeruban@gmail.com>
2022-03-17 22:35:38 +0530

    added styles

--- src/Components/Random/Random.jsx
+++ src/Components/Random/Random.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { myContext } from "../Context/Context";
+import "./random.scss";
 
 const Random = () => {
   const { randomMeal, fetchRandomMeal } = useContext(myContext);
@@ -7,14 +8,22 @@ const Random = () => {
   useEffect(() => {
     fetchRandomMeal();
   }, [fetchRandomMeal]);
+
   return (
     <>
-      <div className="info-cards container">
+      <div className="info-cards container random-container">
+        <h5 style={{ textAlign: "center" }}>
+          Hey there! Generate a random meal here.
+        </h5>
         {randomMeal.map((item) => (
-          <div className="card" style={{ width: "18rem" }} key={item.idMeal}>
+          <div
+            className="card random-card"
+            style={{ width: "18rem" }}
+            key={item.idMeal}
+          >
             <img src={item.strMealThumb} className="card-img-top" alt="..." />
             <h4>{item.strMeal}</h4>
-            <button onClick={fetchRandomMeal}> Generate Another Meal</button>
+            <button onClick={fetchRandomMeal} className="random-btn">Generate Another Meal</button>
             <div className="card-body">
               <h4>Instructions</h4>
               <p className="card-text">{item.strInstructions}</p>
